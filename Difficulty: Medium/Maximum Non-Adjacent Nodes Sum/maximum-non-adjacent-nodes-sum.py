11c69fa99302725af267b545d48e10adf787c59e
Neha-vundavilli2710 <23p31a4272@acet.ac.in>
2025-10-13 19:01:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Non-Adjacent Nodes Sum/maximum-non-adjacent-nodes-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getMaxSum(self, root):
+        def helper(node):
+            if not node:
+                return (0, 0)
+            left = helper(node.left)
+            right = helper(node.right)
+            include = node.data + left[1] + right[1]
+            exclude = max(left) + max(right)
+            return (include, exclude)
+        return max(helper(root))
